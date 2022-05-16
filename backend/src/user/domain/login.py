9ef80bd12c16600e3c53edad3e19d809885b7187
Victor Moraes <victorgabrielvgfm@gmail.com>
2022-05-15 22:40:50 -0300

    Corrige Login para ser desacoplado de UserRepository

--- backend/src/user/domain/login.py
+++ backend/src/user/domain/login.py
@@ -2,13 +2,11 @@ from src.user.domain.ilogin import ILogin
 from src.user.domain.iuser_repository import IUserRepository
 from src.user.domain.user import User
 
-from src.user.adapters.user_repository import UserRepository
-
 ## Classe de domínio
 ## Implementa o serviço de Login
 class Login(ILogin):
     def __init__(self, repository: IUserRepository):
-        self.repository = UserRepository()
+        self.repository = repository
 
     def login(self, username: str, password: str) -> User:
         return self.repository.login(username=username, password=password)
