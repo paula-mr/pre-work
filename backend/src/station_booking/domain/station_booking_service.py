36fc063044d049fd20a2a0664a183a7605d24620
Paula Ribeiro <paula.mribeiro05@gmail.com>
2022-05-29 19:53:58 -0300

    utiliza funcao do dominio no servico

--- backend/src/station_booking/domain/station_booking_service.py
+++ backend/src/station_booking/domain/station_booking_service.py
@@ -21,20 +21,15 @@ class StationBookingService(IStationBooking):
         self.rooms_repository = rooms_repository
 
     def listStationBookings(self, room_id: str, date: datetime) -> List[StationBooking]:
-        station_bookings = self.bookings_repository.listStationBookings(date=date)
+        all_bookings = self.bookings_repository.listStationBookings(date=date)
+        if not room_id:
+            return all_bookings
         
-        if room_id != "":
-            station_bookings_by_room = []
-            station_rooms = self.rooms_repository.listWorkStationRooms(room_id=room_id)
-            if len(station_rooms) != 0:
-                work_station_room = station_rooms[0]
-                for booking in station_bookings:
-                    for room_station in work_station_room.stations:
-                        if booking.station.id == room_station.id:
-                            station_bookings_by_room.append(booking)
-            return station_bookings_by_room
-
-        return station_bookings
+        station_rooms = self.rooms_repository.listWorkStationRooms(room_id=room_id)
+        if len(station_rooms) > 0:
+            work_station_room = station_rooms[0]
+            return work_station_room.get_room_bookings(bookings=station_bookings)
+        return []
 
     def bookStation(self, user_id: str, station_id: UUID, date: datetime) -> None:
         existing_booking = self.bookings_repository.getStationBooking(station_id=station_id, date=date)
