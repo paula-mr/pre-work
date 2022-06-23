3e5ffd83eb8d2e3059f2a8b8413be9d95b543693
Paula Ribeiro <paula.mribeiro05@gmail.com>
2022-06-22 22:21:28 -0300

    ignora arquivos de configuração para coverage

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,10 +1,10 @@
 import dataclasses, json, datetime
 
 class EnhancedJSONEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if dataclasses.is_dataclass(obj):
-                return dataclasses.asdict(obj)
-            elif isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
-                return (str(obj))
-            return super().default(obj)
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        elif isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
+            return (str(obj))
+        return super().default(obj)
             
